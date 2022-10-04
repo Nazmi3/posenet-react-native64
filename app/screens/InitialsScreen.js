ed67feced6c5bee8652676d49767a0b90906de44
Mohd Nazmi bin Man <nazmiryuki@gmail.com>
2022-10-05 02:32:50 +0800

    initial commit

--- app/screens/InitialsScreen.js
+++ app/screens/InitialsScreen.js
@@ -1,8 +1,4 @@
-import React, { Component } from 'react'
-import { View, Image, TouchableOpacity, ImageBackground, InteractionManager, Linking, Platform, SafeAreaView, BackHandler, StatusBar, Alert, Text} from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage'
-import { ScaledSheet } from "react-native-size-matters";
-
+import React, {Component} from 'react';
 
 class InitialScreen extends Component {
     _isMounted = false;
@@ -12,52 +8,45 @@ class InitialScreen extends Component {
 
     componentDidMount() {
         this._isMounted = true;
-        this.props.navigation.push('CameraScreen')
-         
+    this.props.navigation.push('CameraScreen');
     }
-
 
     componentWillUnmount() {
         this._isMounted = false;
     }
 
-
-    
     render() {
         return (
             <View style={styles.container}>
-                <StatusBar
-                    backgroundColor="#00BAF7"
-                    barStyle={'dark-content'}
-                />
+        <StatusBar backgroundColor="#00BAF7" barStyle={'dark-content'} />
                 <View>
                     <Text>Hello</Text>
                 </View>
             </View>
-        )
+    );
     }
 }
 
 const styles = ScaledSheet.create({
     container: {
         backgroundColor: '#00BAF7',
-        flex: 1
+    flex: 1,
     },
     stretch: {
         width: '100%',
-        height: '100%'
+    height: '100%',
     },
     logo: {
         borderRadius: 8,
         width: 250,
         height: 145,
         position: 'relative',
-        marginTop: 0
+    marginTop: 0,
     },
     centerAlign: {
         alignItems: 'center',
-        justifyContent: 'center'
-    }
+    justifyContent: 'center',
+  },
 });
 
 export default InitialScreen;
